9b6b5f5d8a11bf1fea9f0519a2c0a78616c2af9f
Marc Andres <marcandres0923@gmail.com>
2025-08-05 14:49:20 +0800

    Automatically applied pending migrations on startup

--- Program.cs
+++ Program.cs
@@ -22,6 +22,13 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 var app = builder.Build();
 
+// Automatically apply pending migrations on startup
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate(); // <- this is what applies the migrations
+}
+
 if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
