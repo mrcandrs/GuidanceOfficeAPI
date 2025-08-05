86b360e5d8bf9e474384f693ec94531ee74e985b
Marc Andres <marcandres0923@gmail.com>
2025-08-05 12:54:57 +0800

    Add Dockerfile for Railway deployment

--- obj/Debug/net6.0/GuidanceOfficeAPI.AssemblyInfo.cs
+++ obj/Debug/net6.0/GuidanceOfficeAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GuidanceOfficeAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1683f72fef44b8565367b61fcb935a0447eb2d2b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70599b9c7324673aa6f072dc68b18ad23436d865")]
 [assembly: System.Reflection.AssemblyProductAttribute("GuidanceOfficeAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GuidanceOfficeAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
