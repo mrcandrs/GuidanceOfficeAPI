bf974f4e1feaaf753161ba0a9844221553718fac
Marc Andres <marcandres0923@gmail.com>
2025-08-05 12:30:36 +0800

    Fix: disable fallback NuGet folder for Railway build

--- obj/Debug/net6.0/GuidanceOfficeAPI.AssemblyInfo.cs
+++ obj/Debug/net6.0/GuidanceOfficeAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GuidanceOfficeAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e039ac44ad0f944ec77a768f53d7e31914f9cd76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22d86a4c055dd174898846cba74b2e033f384962")]
 [assembly: System.Reflection.AssemblyProductAttribute("GuidanceOfficeAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GuidanceOfficeAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
