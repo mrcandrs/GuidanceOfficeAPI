f5dc0f363ad307291c41cb63a5c8d75f58e00e26
Marc Andres <marcandres0923@gmail.com>
2025-10-17 04:46:31 +0800

    Fixing CPF template

--- obj/Debug/net6.0/GuidanceOfficeAPI.AssemblyInfo.cs
+++ obj/Debug/net6.0/GuidanceOfficeAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GuidanceOfficeAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f091602f4aad085db88fd10479e0181dd69be3a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2cbe8713c6fecd34232e344368a31cd7854578e")]
 [assembly: System.Reflection.AssemblyProductAttribute("GuidanceOfficeAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GuidanceOfficeAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
